cce6f6ccad5246cf3f478da643541dc3ebf4f819
SAQIB\hp <muhammadsaqib.ms228@gmail.com>
2021-03-23 16:06:07 +0500

    testing new theme

--- EProctor/Models/AppDbContext.cs
+++ EProctor/Models/AppDbContext.cs
@@ -18,5 +18,16 @@ namespace EProctor.Models
 
         public object Course { get; internal set; }
         public DbSet<Course> Courses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //this will be used to dont delete entitis with foreign key
+            //foreach(var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(e=>e.GetForeignKeys()))
+            //{
+            //    foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            //}
+
+        }
     }
 }
